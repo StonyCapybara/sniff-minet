fc7045b424bb19384002f7381cdc9fda66424ef7
StonyCapybara <97652916+StonyCapybara@users.noreply.github.com>
2024-11-04 00:03:54 +0530

    Update page.tsx

--- src/app/chatrooms/page.tsx
+++ src/app/chatrooms/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 
 export default function Chatrooms() {
-  return <div className="px-12">
+  return <div className="p-12">
     <div className="dblue-shadow bg-lblue rounded-lg max-w-7xl m-auto mt-8 w-fit border-2 border-dblue min-h-[500px]">
       <div className="w-full bg-[#4D6192] text-lblue font-barlow font-semibold text-6xl rounded-t-lg px-6 py-2">CHATROOMS</div>
       <div className="flex w-full justify-stretch items-stretch">
